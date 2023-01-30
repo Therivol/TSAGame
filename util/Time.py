b9fd76b0bda0d019230aea5170b0aea7ae36acb8
Zander <alex@barba.org>
2023-01-29 22:10:22 -0700

    test

--- util/Time.py
+++ util/Time.py
@@ -1,8 +1,36 @@
 import math
+import pygame
 import time
 
 
 class Time:
+    time_started = 0
+    delta_time_ns = 0
+    last_time = 0
+
+    @staticmethod
+    def awake():
+        Time.time_started = pygame.time.get_ticks()
+        Time.last_time = pygame.time.get_ticks()
+
+    @staticmethod
+    def start_frame():
+        Time.last_time = pygame.time.get_ticks()
+
+    @staticmethod
+    def calculate_dt():
+        Time.delta_time_ns = (pygame.time.get_ticks() - Time.last_time)
+
+    @staticmethod
+    def delta():
+        return Time.delta_time_ns / 1000
+
+    @staticmethod
+    def elapsed():
+        return (pygame.time.get_ticks() - Time.time_started) / 1000
+
+
+class OldTime:
     time_started = 0
     delta_time_ns = 0
     last_time = 0
